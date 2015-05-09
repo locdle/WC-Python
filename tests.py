766ac963f8a4493cc9df0b497549c45ddae2a2f1
locdle <ducloc1803@gmail.com>
2015-05-09 09:35:08 -0700

    implement count bytes

---
+++ tests.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+__author__ = 'locle'
+
+import unittest
+import subprocess
+import re
+
+
+class TestWC(unittest.TestCase):
+    def test_count_bytes_in_one_file(self):
+        system_result = subprocess.getoutput("wc -c test.txt").lstrip()
+        output = subprocess.getoutput("python3 wc.py -c test.txt")
+        self.assertEqual(output, system_result)
+
+    def test_another_count_bytes_in_one_file(self):
+        system_result = subprocess.getoutput("wc -c test1.txt").lstrip()
+        output = subprocess.getoutput("python3 wc.py -c test1.txt")
+        self.assertEqual(output, system_result)
+
+    def test_count_bytes_in_two_files(self):
+        system_result = re.sub('  +', '', subprocess.getoutput("wc -c test.txt test1.txt"))
+        output = subprocess.getoutput("python3 wc.py -c test.txt test1.txt")
+        self.assertEqual(output, system_result)
+
+
+if __name__ == '__main__':
+    unittest.main()
