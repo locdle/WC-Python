f0b7bab28615cb8c2ecc348b72a5b391871a57b1
locdle <ducloc1803@gmail.com>
2015-05-09 11:46:02 -0700

    add more documentary

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 [![Build Status](https://travis-ci.org/locdle/WC-Python.svg?branch=master)](https://travis-ci.org/locdle/WC-Python)
 
+##WC
 Re-create of the Unix wc command is default in GNU coreutils package using python
+
+##usage
+
+wc.py [-c | -l | -m | -w] filename [filename] 
+print the number of lines, words, and bytes contained in each input file, or standard input
+
+optional arguments:
+
+    -h      show this help message and exit     
+    -c      show the number of bytes in each input file is written to the standard output    
+    -l      show the number of lines in each input file is written to the standard output
+    -m      show the number of characters in each input file is written to the standard output
+    -w      show the number of words in each input file is written to the standard output
 
--- wc.py
+++ wc.py
@@ -5,34 +5,37 @@ import os
 
 __author__ = 'locle'
 
-
+# check file exist and handle IO exception
 def open_file(filename):
     try:
         fin = open(filename, 'r')
-        text = fin.read()
-        fin.close()
     except IOError:
         print("File %s not found" % filename)
         raise SystemExit
 
 
+# count number of bytes in a file
 def count_bytes(filename):
     open_file(filename)
     getsize = os.path.getsize(filename)
     return getsize
 
 
+# count number of lines in a file
 def count_lines(filename):
     open_file(filename)
     number_of_lines = len(open(filename).readlines())
     return number_of_lines
 
 
+# count number of character in a file
 def count_character(filename):
     open_file(filename)
     number_of_character = len(open(filename).read())
     return number_of_character
 
+
+# count number of words in a file
 def count_words(filename):
     open_file(filename)
     number_of_words = len(open(filename).read().split(None))
@@ -40,11 +43,13 @@ def count_words(filename):
 
 
 def main():
+    # local count number of total if users input multiple file
     count_byte = 0
     count_line = 0
     count_char = 0
     count_word = 0
 
+    # flag option
     byte = False
     line = False
     char = False
@@ -52,7 +57,20 @@ def main():
     option = True
 
     if len(sys.argv) >= 2:
-        if sys.argv[1] == "-c":
+        if sys.argv[1] == "-h":
+            print("The wc utility displays the number of lines, words, and bytes contained "
+                  "in each input file, or standard input.\n"
+                  "The following options are available:\n"
+                  "\t-c\t      The number of bytes in each input file is written to the standard"
+                  "output.  This will cancel out any prior usage of the -m option.\n"
+                  "\t-l\t      The number of lines in each input file is written to the standard output.\n"
+                  "\t-m\t      The number of characters in each input file is written to the standard output.\n"
+                  "\t-w\t      The number of words in each input file is written to the standard output.\n"
+                  "When an option is specified, wc only reports the information requested by"
+                  "that option.\nThe order of output always takes the form of line, word,"
+                  "byte, and file name.\nThe default action is equivalent to specifying the"
+                  "-c, -l and -w options.")
+        elif sys.argv[1] == "-c":
             byte = True
             for filename in sys.argv[2:]:
                 print("%d %s" % (count_bytes(filename), filename))
